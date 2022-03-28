077e6d3b562895a911f882e0d4126e2f01d82325
Leticia Salazar <leticia.salazar64@spsmail.cuny.edu>
2022-03-28 19:28:16 -0400

    changes

---
+++ Spyder/PythonBasics.py
@@ -0,0 +1,29 @@
+# Hello world
+```
+print("hello world!")
+```
+
+# Variables & Datatypes
+```
+x = 'hello world!'
+a = 5
+i = False
+o = True
+f = 1.55
+f = 'what1'
+```
+
+```
+print(type(o))
+
+```
+
+# Loops
+
+# Functions
+
+# Conditions
+
+# Input
+
+# Putting it together
